22ee8e6ac0ac12a95eadb5e6b12bcd4920788472
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2022-07-11 20:51:45 +0300

    Repositories added

---
+++ BackendDemo.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackendDemo.Core.Entities;
+
+namespace BackendDemo.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T:class,IEntity,new()
+    {
+        IQueryable<T> Table { get; }
+
+        
+    }
+
+
+
+
+}
