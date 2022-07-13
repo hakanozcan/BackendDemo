a3253a24128b04fb689f43041213e16b0f7a058c
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2022-07-13 18:01:59 +0300

    Abstract and concrete classes added

---
+++ BackendDemo.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackendDemo.Core.DataAccess;
+using BackendDemo.Northwind.Entities.Concrete;
+
+namespace BackendDemo.Northwind.DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+
+    }
+}
---
+++ BackendDemo.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BackendDemo.Core.DataAccess.EntityFramework;
+using BackendDemo.Northwind.DataAccess.Abstract;
+using BackendDemo.Northwind.Entities.Concrete;
+
+namespace BackendDemo.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal:EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    {
+
+
+    }
